6ed017aea444180b7bdc8e7a273cbc8334a8a848
Robert Crowdis <RobertCrowdis@gmail.com>
2020-05-15 17:18:10 -0400

    feat(material): add layout service to swap between two themes

--- src/app/core/services/layout.service.ts
+++ src/app/core/services/layout.service.ts
@@ -1,34 +1,50 @@
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
 
 
 /**
- * Services that allows setting of the style of the application, between Material Design and the original website design.
+ * Handles the layout of the application between either Material Design and the original website design.
  */
 @Injectable({
   providedIn: 'root'
 })
 export class LayoutService {
 
-  constructor() { }
+  private _layout$: ReplaySubject<string> = new ReplaySubject<string>();
+
+  constructor() {
+    this.instantiateLayout();
+  }
+
+
+  get layout() {
+    return this._layout$;
+  }
 
   /**
    * Sets a string inside of the browser's local storage session, to save styling between visits of the website.
    *
    * @param layoutType String containing 'material' or 'custom'. Default is 'material'
    */
-  public setLayout(layoutType: string = 'material'): void {
+  public setLayout(layoutType: string): void {
     localStorage.setItem('layout', layoutType);
+    this._layout$.next(layoutType);
   }
 
   /**
-   * Fetches the current
+   * Fetches the user's last selected layout preference from local storage and sets the website style accordingly.
+   * If no layout is found, then the default will be set as 'custom' in local storage and in the application.
    */
-  public getLayout(): string {
-    const layout = localStorage.getItem('layout');
-    console.log('Got layout: ' + layout);
-    console.log(typeof layout);
-    // if (layout)
-    return String(layout);
+  public instantiateLayout(): void {
+    const layout = String(localStorage.getItem('layout'));
+    if (layout === 'material') {
+      this._layout$.next('material');
+    } else {
+      if (layout === 'null' || layout === 'undefined') {
+        localStorage.setItem('layout', 'custom');
+      }
+      this._layout$.next('custom');
+    }
   }
 
 }
