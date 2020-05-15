e5306257ce649be1d05883a9fdb966e750bb6fb5
Robert Crowdis <RobertCrowdis@gmail.com>
2020-05-15 17:19:41 -0400

    fix(styling): add new wrapper for parent html component
    This styling will be used when adding new header and footer elements, along with multiple pages (bio, projects, about, blog, ect)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { LayoutService } from './core/services/layout.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+
+  private _layoutSubscription$: Subscription;
+  public layoutStyle: string;
+
+  /**
+   * @param _ls Handles the layout of the application between either Material Design and the original website design.
+   */
+  constructor(private _ls: LayoutService) {}
+
+  ngOnInit() {
+    this._layoutSubscription$ = this._ls.layout.subscribe((layout: string) => {
+      this.layoutStyle = layout;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this._layoutSubscription$) { this._layoutSubscription$.unsubscribe(); }
+  }
+}
