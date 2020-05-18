03d2d1fa6775ff2f24fce144bb1d7888af29d147
Robert Crowdis <RobertCrowdis@gmail.com>
2020-05-18 11:25:38 -0400

    feat(pages): add routing blueprint and title updates on page route (#4)
    * feat(about): move app component into new child component
    
    * Add routing for children components, so multiple pages can be navigated between
    
    * feat(title): allow title of webpage to update on route change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AboutComponent,
+    data: { title: 'About' }
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    data: { title: 'About' }
+  },
+  { // TODO: Swap this route out with the proper page once it is completed - RC 5/18/20
+    path: 'work',
+    component: AboutComponent,
+    data: { title: 'Work' }
+  },
+  {
+    path: '**',
+    component: AboutComponent,
+    data: { title: 'DNE' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
 import { LayoutService } from './core/services/layout.service';
+
 import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -10,14 +16,40 @@ import { Subscription } from 'rxjs';
 export class AppComponent implements OnInit, OnDestroy {
 
   private _layoutSubscription$: Subscription;
+  private _defaultTitle = 'RC';
+
+
   public layoutStyle: string;
 
   /**
    * @param _ls Handles the layout of the application between either Material Design and the original website design.
+   * @param _route Provides access to information about a route associated with a component that is loaded in an outlet.
+   * @param _router A service that provides navigation and URL manipulation capabilities.
+   * @param _ts A service that can be used to get and set the title of a current HTML document.
    */
-  constructor(private _ls: LayoutService) {}
-
+  constructor(private _ls: LayoutService, private _route: ActivatedRoute, private _router: Router, private _ts: Title) { }
   ngOnInit() {
+
+    // Subscribe to the router and provide updates to the document title based on the current route.
+    this._router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let child = this._route.firstChild;
+        while (child.firstChild) {
+          child = child.firstChild;
+        }
+        if (child.snapshot.data['title']) {
+          return child.snapshot.data['title'];
+        } else {
+          return this._defaultTitle;
+        }
+      })
+    ).subscribe((pageTitle: string) => {
+      const fullTitle = `${pageTitle} | Robert Crowdis`;
+      this._ts.setTitle(fullTitle);
+    });
+
+    // Subscribe to layout changes and pass the changes to children components
     this._layoutSubscription$ = this._ls.layout.subscribe((layout: string) => {
       this.layoutStyle = layout;
     });
