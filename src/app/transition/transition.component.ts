e922b818c7596aedd5a023f5a06a5550085f709d
Robert Crowdis <RobertCrowdis@gmail.com>
2020-05-18 15:17:11 -0400

    feat(transition): add transition overlay and attach to layout state

---
+++ src/app/transition/transition.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, transition, animate, sequence } from '@angular/animations';
+
+
+/**
+ * Overlay to display when a user is transitioning between different styles on the website.
+ */
+@Component({
+  selector: 'app-transition',
+  templateUrl: './transition.component.html',
+  styleUrls: ['./transition.component.scss'],
+  animations: [
+    trigger('fadeAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(250),
+      ]),
+      transition(':leave', [
+        animate(500, style({ opacity: 1 })),
+        animate(250, style({ opacity: 0 }))
+      ])
+    ])
+  ]
+})
+export class TransitionComponent implements OnInit {
+
+  @Input() showOverlay: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
