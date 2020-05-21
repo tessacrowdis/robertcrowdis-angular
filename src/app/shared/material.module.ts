546064386677954d21f81c3db3b1f4da5b69b295
Robert Crowdis <RobertCrowdis@gmail.com>
2020-05-21 14:06:56 -0400

    feat(material): add new material imports

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-
 
 /**
  * Module that imports and manages Angular Material modules
@@ -17,6 +19,9 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatButtonModule,
     MatCardModule,
     MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatSidenavModule,
     MatToolbarModule,
     MatTooltipModule,
   ],
@@ -24,6 +29,9 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatButtonModule,
     MatCardModule,
     MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatSidenavModule,
     MatToolbarModule,
     MatTooltipModule,
   ]
